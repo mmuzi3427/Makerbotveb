550843c77df57b11bf087b2b422b13f62cf1dae7
PyDev <mmuzi3427@gmail.com>
2025-06-08 15:52:57 +0500

    Update sub.js

--- sub.js
+++ sub.js
@@ -3,12 +3,12 @@ async function telegramBotAPI(token) {
     const apiUrl = `https://api.telegram.org/bot${token}`;
 
     try {
-        const response = await fetch(apiUrl+"getchatmember");
+        const response = await fetch(apiUrl+"getChatMember");
         const data = await response.json();
 
         if (data.code === 200) {
-            const t = data.data.timings;
-            return [t.Fajr, t.Sunrise, t.Dhuhr, t.Asr, t.Maghrib, t.Isha];
+            const t = data;
+            return t//[t.Fajr, t.Sunrise, t.Dhuhr, t.Asr, t.Maghrib, t.Isha];
         } else {
             return ["Xatolik: ma’lumot topilmadi"];
         }
